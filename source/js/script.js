3d15d04f3808567790b396a02332c09bdfb7caa7
MarySlip <androsov.evgeny1@gmail.com>
2023-02-07 11:38:12 +0200

    my projekt sushi

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('.header-menu__icon').click(function (event) {
+		$('.header-menu__icon,.header-top-lang,.header-bottom-menu').toggleClass('active');
+		$('body').toggleClass('lock')
+	});
+});
+
+
+
+
+<script src="http://maps.google.com/maps/api/js?sensor=false&amp;key="></script>
